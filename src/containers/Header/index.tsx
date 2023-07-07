e1da1fc3ed36b1d84c6cc2ef440b2771b3ef5973
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-06 23:33:09 -0300

    feat: add header container

---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Subheader } from "@/components/Subheader";
+import { Container } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Subheader />
+    </Container>
+  );
+}
---
+++ src/containers/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  min-height: 44.875rem;
+  background-image: url("assets/images/background-header.png");
+  background-size: cover;
+  background-position: bottom center;
+
+  display: flex;
+  flex-direction: column;
+`;
