a1806095ba312c7346da1f5fbb0e810192ae2c6d
Werllety Gonçalves <werlley.registros@hotmail.com>
2023-07-08 20:56:37 -0300

    feat: add profile in header container

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,10 +1,12 @@
 import { Subheader } from "@/components/Subheader";
 import { Container } from "./styles";
+import { Profile } from "@/components/Profile";
 
 export function Header() {
   return (
     <Container>
       <Subheader />
+      <Profile />
     </Container>
   );
 }
