22a228905ced0d971fba4662b750f561e1e0361a
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-14 22:18:12 -0300

    feat: create skill container

---
+++ src/containers/Skills/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode, useState } from "react";
+
+import { Container, DivContent } from "./styles";
+import { Title } from "@/components/Title";
+
+export function Skills() {
+  ]);
+
+  return (
+    <Container>
+      <Title>Habilidades</Title>
+    </Container>
+  );
+}
---
+++ src/containers/Skills/styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: min(100%, 63rem);
+  margin-inline: auto;
+`;
+
