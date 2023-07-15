9d96fdc700e7cdf77ef7b9a53308338d4d35863e
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-14 22:39:13 -0300

    feat: add skill container layout and styles

--- src/containers/Skills/index.tsx
+++ src/containers/Skills/index.tsx
@@ -1,14 +1,53 @@
 import { ReactNode, useState } from "react";
 
-import { Container, DivContent } from "./styles";
+import { Container, DivContent, Li, UlColumn } from "./styles";
 import { Title } from "@/components/Title";
 
 export function Skills() {
-  ]);
+  const [skillData] = useState({
+    firstColumnData: [
+      "+6 anos de experiência com HTML, CSS e JavaScript;",
+      "Desenvolvimento de sistemas WEB responsivos e aplicações Mobile;",
+      "Designer WEB/Mobile (UI) e User Experience (UX);",
+      "CI/CD pipelines com GitHub Actions;",
+      "Desenvolvimentos Front-end seguindo o conceito de Pixel Perfection com Figma;",
+      "Versionamento de projetos usando Conventional Commits com GitHub;",
+      "Desenvolvimento com metodologias ágeis;",
+      "Ferramentas de SEO como Google Search Console e Clarity;",
+      "Utilização de serviços como Digital Ocean, Amazon WEB Services (AWS) e Vercel;",
+    ],
+    secondColumnData: [
+      "Utilização de softwares como Visual Studio Code, AutoCAD, ferramentas do Office;",
+      "Experiência em plataformas para gerenciamento de projetos como ClickUp, Asana e Trello;",
+      "Experiência em eletrica e eletrônica em geral; ",
+      "Participativo, comunicativo, gosta de trabalhar em equipe e sempre aberto a novas experiências; ",
+      "Amante de tecnologia e inovação!",
+    ],
+  });
 
   return (
     <Container>
       <Title>Habilidades</Title>
+      <DivContent>
+        <UlColumn>
+          {skillData.firstColumnData.map((skill) => (
+            <Li key={skill}>{skill}</Li>
+          ))}
+        </UlColumn>
+        <UlColumn>
+          {skillData.secondColumnData.map((skill, idx) => (
+            <Li
+              key={skill}
+              style={{
+                fontWeight:
+                  idx === skillData.secondColumnData.length - 1 ? "700" : "300",
+              }}
+            >
+              {skill}
+            </Li>
+          ))}
+        </UlColumn>
+      </DivContent>
     </Container>
   );
 }
--- src/containers/Skills/styles.ts
+++ src/containers/Skills/styles.ts
@@ -3,5 +3,36 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   width: min(100%, 63rem);
   margin-inline: auto;
+  margin-top: 4rem;
+  padding-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
+export const DivContent = styled.div`
+  display: flex;
+  padding: 3rem 2rem;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 4rem;
+  border-radius: var(--border-radius);
+  background: linear-gradient(
+    180deg,
+    var(--dark-gray-color) 0%,
+    rgba(var(--background-color-rgb), 0) 100%
+  );
+`;
+
+export const UlColumn = styled.ul`
+  flex: 1;
+`;
+
+export const Li = styled.li`
+  color: var(--white-color);
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 140%;
+  margin-left: 1.125rem;
+`;
