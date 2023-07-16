4772d5d1f1ab9871baebff30d21a2b626d9b8212
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-16 17:05:57 -0300

    feat: create QuickRead container layout and styles

--- src/containers/QuickRead/index.tsx
+++ src/containers/QuickRead/index.tsx
@@ -1,11 +1,34 @@
-import { ReactNode } from "react";
+import { useCallback, useState } from "react";
 
-import { Container } from "./styles";
+import { Container, DivDeck } from "./styles";
+import { Title } from "@/components/Title";
+
+// images
+import Post01Image from "../../../public/assets/images/quick-read/post-1.jpg";
+
+import { IQuickReadPost } from "@/dtos/quickRead";
+import { CardQuickRead } from "@/components/CardQuickRead";
 
 export function QuickRead() {
+  const [postsData] = useState<IQuickReadPost[]>([
+    {
+      title: "Síndrome do impostor",
+      image: Post01Image,
+      published_at: "2023-06-19T03:21:02.604Z",
+      description:
+        'Fala pessoal, tudo bom? Hoje venho conversar com vocês sobre algo que já aconteceu bastante comigo e pode ser que aconteça com mais alguém. Já ouviram falar sobre "Síndrome do impostor"? Se não, comentarei um pouco com vocês.',
+      link: "https://www.linkedin.com/feed/update/urn:li:activity:7076365932083691520/",
+    },
+  ]);
+
   return (
     <Container>
-      <h1>QuickRead</h1>
+      <Title>Leitura Rápida</Title>
+      <DivDeck>
+        {postsData.map((post) => (
+          <CardQuickRead {...post} key={post.title} />
+        ))}
+      </DivDeck>
     </Container>
   );
 }
