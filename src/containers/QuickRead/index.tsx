6eba6d0c1a3a0f440698f3bf6a13be30fa47472a
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-15 23:29:53 -0300

    feat: create QuickRead container

---
+++ src/containers/QuickRead/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+import { Container } from "./styles";
+
+export function QuickRead() {
+  return (
+    <Container>
+      <h1>QuickRead</h1>
+    </Container>
+  );
+}
