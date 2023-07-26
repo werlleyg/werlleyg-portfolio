1e28837c0b393fbccb331c7892841e825f5a20b3
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-25 23:24:26 -0300

    feat: create ICardMyProject interface

---
+++ src/dtos/myProjects.ts
@@ -0,0 +1,10 @@
+import { StaticImageData } from "next/image";
+
+export interface ICardMyProject {
+  title: string;
+  image: StaticImageData;
+  description: string;
+  link: string;
+  skills: string[];
+  technologies: string[];
+}
