7617abb2cc96aacca1554d6bdfff704a63ed6d56
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-09-24 17:43:58 -0300

    feat: create section title

--- src/containers/GithubProjects/index.tsx
+++ src/containers/GithubProjects/index.tsx
@@ -1,6 +1,15 @@
 // styles
-import { Container } from "./styles";
+import { Container, DivTitle, H1 } from "./styles";
+// assets
+import Image from "next/image";
+import GithubIcon from "../../../public/assets/icons/github.svg";
 
 export function GithubProjects() {
-  return <Container>github projects</Container>;
+  return (
+    <Container>
+      <DivTitle>
+        <Image src={GithubIcon} alt="GitHub Icon" width={48} /> <H1>GitHub</H1>
+      </DivTitle>
+    </Container>
+  );
 }
--- src/containers/GithubProjects/styles.ts
+++ src/containers/GithubProjects/styles.ts
@@ -1,3 +1,20 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: min(100% - 3rem, var(--inner-max-width-in));
+  margin-inline: auto;
+  margin-top: 6rem;
+`;
+
+export const DivTitle = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const H1 = styled.h1`
+  color: var(--white-color);
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
